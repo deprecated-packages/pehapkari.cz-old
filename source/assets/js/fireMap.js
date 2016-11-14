27d198953b6168c27c898337bd1c5f1818a4116b
Tomáš Kazatel <kazatel@jiktogroup.cz>
2016-11-14 20:50:17 +0100

    Finding PHP Friends v.2
    Subtract to two files
    Fix some little bugs

--- source/assets/js/fireMap.js
+++ source/assets/js/fireMap.js
@@ -13,6 +13,8 @@ firebase.initializeApp({
     storageBucket: "pehapkari.appspot.com"
 });
 
+firebase.auth().signInAnonymously().catch();
+
 /**
  * get setting for google map
  * @param integer zoom
@@ -21,8 +23,9 @@ firebase.initializeApp({
 function crSettings(zoom) {
     return {
         zoom : zoom,
-        center: {lat: 49.817, lng: 15.472},
+        center: {lat: 49.7437652, lng: 15.3364628},
         streetViewControl: false,
+        scrollwheel:  false,
     }
 }
 
@@ -30,8 +33,16 @@ function crSettings(zoom) {
  * parent function loading maps after async load google maps api
  */
 function initMaps() {
-    initMap();
-    initFormMap();
+
+
+    if(document.getElementById("map")) {
+        initMap();
+    }
+
+    if(document.getElementById("formMap")) {
+        initFormMap();
+    }
+
 }
 
 /**
@@ -44,12 +55,57 @@ function initFormMap() {
 }
 
 /**
+ * set info box for google marker
+ * @param object user
+ * @returns {string}
+ */
+function setInfoBox(user) {
+
+    var controll = false;
+
+    var pom = "<p>"
+            + "<strong>" + user.name + " " + user.surname + " " + ((user.nickname !== undefined) ? "(" + user.nickname + ")" : "") + "</strong>"
+            + "<br />"
+            + "<a href='mailto:" + user.email + "'>" + user.email + "</a>"
+            + "<br /><br />";
+
+    if(user.web !== undefined) {
+        pom += "<a href='" + user.web + "' target='_blank'>Webovky</a> | ";
+        controll = true;
+    }
+
+    if(user.twitter !== undefined) {
+        pom += "<a href='http://twitter.com/" + user.twitter + "' target='_blank'>Twitter</a> | ";
+        controll = true;
+    }
+
+    if(user.github !== undefined) {
+        pom += "<a href='http://github.com/" + user.github + "' target='_blank'>GitHub</a> | ";
+        controll = true;
+    }
+
+    if(controll) {
+        pom = pom.substr(0, pom.length - 3);
+    }
+
+    pom += "</p>";
+
+    return pom;
+
+}
+
+/**
  * init big google map to global variable to latest manipulation
  */
 function initMap() {
 
+    //zoom set about width
+    var z = (window.innerWidth < 500) ? 6 : 7 ;
+
     //init map
-    map = new google.maps.Map(document.getElementById('map'), crSettings(7));
+    map = new google.maps.Map(document.getElementById('map'), crSettings( z ));
+
+    var info = new google.maps.InfoWindow();
 
     //fetch all data from firebase
     firebase.database().ref('users').once('value').then(function(snapshot) {
@@ -63,31 +119,17 @@ function initMap() {
             //create google map marker
             var marker = new google.maps.Marker({
                 position: {lat : user.lat, lng: user.lng},
-                map: map,
-            });
-
-            //check user phone empty
-            var tel = "";
-
-            if(user.tel !== undefined) {
-                tel += "<br /><a href='tel:"+user.tel+"' title='"+user.tel+"'>"+user.tel+"</a>"
-            }
-
-            // set google marker info with full name, mail, maybe phone
-            var info = new google.maps.InfoWindow({
-                content:
-                "<p>" +
-                "<strong>"+user.name+" "+user.surname+"</strong>" +
-                "<br />" +
-                "<a href='mailto:"+user.email+"' title='"+user.email+"'>"+user.email+"</a>" +
-                tel +
-                "</p>"
+                map: map
             });
 
             //set event listener to show info box after click on marker
-            google.maps.event.addListener(marker, "click", function(e) {
-                info.open(map, this);
-            });
+            google.maps.event.addListener(marker, "click", (function(marker, user) {
+                return function() {
+                    info.close();
+                    info.setContent(setInfoBox(user));
+                    info.open(map, marker);
+                }
+            })(marker, user));
 
         }
 
@@ -136,143 +178,147 @@ function validateInput(input, validate) {
     }
 }
 
-// work after load window
-window.onload = function() {
+/**
+ * search coordinate from google maps api
+ */
+function search() {
 
-    //add listener to click on search button
-    document.getElementById("search").addEventListener("click", function() {
+    //trigger city name
+    var city = document.getElementById('city');
 
-        //trigger city name
-        var city = document.getElementById('city');
+    //disabling input for long load
+    city.disabled = true;
 
-        //disabling input for long load
-        city.disabled = true;
+    //change button to loading icon for long load
+    this.innerHTML = "<em class='fa fa-refresh fa-spin fa-fw'></em>";
 
-        //change button to loading icon for long load
-        this.innerHTML = "<em class='fa fa-refresh fa-spin fa-fw'></em>";
+    //when form map marker is exist remove it
+    if(formMapMarker != null) {
+        formMapMarker.setMap(null);
+    }
 
-        //when form map marker is exist remove it
-        if(formMapMarker != null) {
-            formMapMarker.setMap(null);
-        }
+    //ajax call to google maps api to get lat, lng values for city value
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://maps.google.com/maps/api/geocode/json?address=" + city.value);
+    xhr.onload = function() {
 
-        //ajax call to google maps api to get lat, lng values for city value
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://maps.google.com/maps/api/geocode/json?address=" + city.value);
-        xhr.onload = function() {
+        //when get response from google maps api
+        if(xhr.status === 200) {
 
-            //when get response from google maps api
-            if(xhr.status === 200) {
+            //parse response to json for simple usage
+            var response = JSON.parse(xhr.responseText);
 
-                //parse response to json for simple usage
-                var response = JSON.parse(xhr.responseText);
+            //check if json not empty
+            if(response.results[0] != undefined) {
 
-                //check if json not empty
-                if(response.results[0] != undefined) {
+                //set lat, lng to global newUser variable
+                newUser['lat'] = response.results[0].geometry.location.lat;
+                newUser['lng'] = response.results[0].geometry.location.lng;
 
-                    //set lat, lng to global newUser variable
-                    newUser['lat'] = response.results[0].geometry.location.lat;
-                    newUser['lng'] = response.results[0].geometry.location.lng;
+                //add new marker to small form map
+                formMapMarker = new google.maps.Marker({
+                    position: response.results[0].geometry.location,
+                    map: formMap
+                });
 
-                    //add new marker to small form map
-                    formMapMarker = new google.maps.Marker({
-                        position: response.results[0].geometry.location,
-                        map: formMap
-                    });
+                //change adress input to sucess
+                inputChanger(city.parentNode, false);
 
-                    //change adress input to sucess
-                    inputChanger(city.parentNode, false);
-
-                } else {
-
-                    //change adress input to error status
-                    inputChanger(city.parentNode, true);
-
-                }
             } else {
 
                 //change adress input to error status
                 inputChanger(city.parentNode, true);
 
             }
-        };
-        xhr.send();
+        } else {
 
-        //revert text to find button
-        this.innerText = 'Najdi!';
+            //change adress input to error status
+            inputChanger(city.parentNode, true);
 
-        //revert disabling input after load and execute data
-        city.disabled = false;
+        }
+    };
+    xhr.send();
 
-    });
+    //revert text to find button
+    this.innerHTML = 'Najdi!';
 
-    //add listener to click on submit form button
-    document.getElementById("fire").addEventListener("click", function() {
+    //revert disabling input after load and execute data
+    city.disabled = false;
 
-        //validation all input from form
-        var name = validateInput(document.getElementById("name"),true);
-        var surname = validateInput(document.getElementById("surname"),true);
-        var email = validateInput(document.getElementById("email"),true);
-        var tel = validateInput(document.getElementById("tel"),false);
+}
 
-        //check if validation of required inputs is ok
-        if(name && surname && email) {
+if(document.getElementById("formMap")) {
 
-            //check if is set new user lat and lng
-            if(newUser.lat !== null && newUser.lng !== null) {
+    // work after load window
+    window.onload = function() {
 
-                //fetch latest id from firebase
-                firebase.database().ref('users').orderByKey().limitToLast(1).once("value").then(function(snapshot) {
+        //add listener to click on search button
+        document.getElementById("search").addEventListener("click", search);
 
-                    //foreach data from fireabse response
-                    snapshot.forEach(function(data) {
+        //add listener to on blur from city input
+        document.getElementById("city").addEventListener("blur", search);
 
-                        //add new user to fireabse
-                        var userId = parseInt(data.key) + 1 ;
-                        firebase.database().ref('users/' + userId).set(newUser);
+        //add listener to click on submit form button
+        document.getElementById("fire").addEventListener("click", function() {
+
+            //validation all input from form
+            //required
+            var name = validateInput(document.getElementById("name"),true);
+            var surname = validateInput(document.getElementById("surname"),true);
+            var email = validateInput(document.getElementById("email"),true);
+
+            //others
+            var nickname = validateInput(document.getElementById("nickname"),false);
+            var web = validateInput(document.getElementById("web"),false);
+            var twitter = validateInput(document.getElementById("twitter"),false);
+            var github = validateInput(document.getElementById("github"),false);
+
+            //check if validation of required inputs is ok
+            if(name && surname && email) {
+
+                //check if is set new user lat and lng
+                if(newUser.lat !== null && newUser.lng !== null) {
+
+                    //fetch latest id from firebase
+                    firebase.database().ref('users').orderByKey().limitToLast(1).once("value").then(function(snapshot) {
+
+                        //foreach data from fireabse response
+                        snapshot.forEach(function(data) {
+
+                            //add new user to fireabse
+                            var userId = parseInt(data.key) + 1 ;
+                            firebase.database().ref('users/' + userId).set(newUser);
+
+                        });
 
                     });
 
-                });
 
-                //add new marker to big map from actual data
-                var marker = new google.maps.Marker({
-                    position: {lat: newUser.lat, lng: newUser.lng},
-                    map: map
-                });
+                    document.getElementById("formRem").innerHTML =
+                        "<div class='col-sm-10 col-sm-offset-1 col-xs-12'>" +
+                            "<div class='alert alert-success'>" +
+                                "<strong>Výborně "+ newUser.name +"!</strong> Tvůj profil už je na mapě." +
+                            "</div>" +
+                        "</div>" +
+                        "<div class='col-sm-10 col-sm-offset-1 col-xs-12 text-center'>" +
+                            "<a href='/kde-najdes-php-kamose' class='btn btn-lg btn-info' role='button'>" +
+                                "<em class='fa fa-arrow-left fa-fw'></em> Zpět na mapu" +
+                            "</a>" +
+                        "</div>";
 
-                //add info box to new marker from actual data
-                var info = new google.maps.InfoWindow({
-                    content:
-                    "<p>" +
-                    "<strong>"+newUser.name+" "+newUser.surname+"</strong>" +
-                    "<br />" +
-                    "<a href='mailto:"+newUser.email+"' title='"+newUser.email+"'>"+newUser.email+"</a>" +
-                    "</p>"
-                });
+                    //delete global new user data
+                    delete newUser;
 
-                //add listener to show new marker info box
-                google.maps.event.addListener(marker, "click", function(e) {
-                    info.open(map, this);
-                });
+                } else {
 
-                //replace form with map for succes alert message
-                document.getElementById("formRem").innerHTML =
-                    "<div class='alert alert-success text-center' role='alert'>" +
-                    "<span><strong>Paráda!</strong> Už jsi mezi námi</span>" +
-                    "</div>";
+                    //if data lat and lng not set set adress input to error
+                    inputChanger(document.getElementById("city", true));
 
-                //delete global new user data
-                delete newUser;
-
-            } else {
-
-                //if data lat and lng not set set adress input to error
-                inputChanger(document.getElementById("city", true));
-
+                }
             }
-        }
 
-    });
+        });
 
-};
+    };
+
+}
