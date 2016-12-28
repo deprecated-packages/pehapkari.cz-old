3e9730a95cc2aa34b79aab74118f532dc86a49f3
TomasVotruba <tomas.vot@gmail.com>
2016-12-28 01:55:24 +0100

    [blog] add Symfony Console tested article

---
+++ src/Posts/Year2016/SymfonyConsole/Command/HashPasswordCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Pehapkari\Website\Posts\Year2016\SymfonyConsole\Command;
+
+use Nette\Security\Passwords;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class HashPasswordCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('hash-password');
+        $this->setDescription('Hashes provided password with BCRYPT and prints to output.');
+        $this->addArgument('password', InputArgument::REQUIRED, 'Password to be hashed.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $password = $input->getArgument('password');
+
+        $hashedPassword = Passwords::hash($password);
+
+        $output->writeln(sprintf(
+            'Your hashed password is: <info>%s</info>', $hashedPassword
+        ));
+
+        return 0;
+    }
+}
