03f1eb076e6695b2f1be27709fa564e04005dd89
Tomas Pilar <email@tomaspilar.cz>
2017-01-13 17:30:07 +0100

    add main ListeningNetteComponentsTest

---
+++ tests/Posts/Year2017/ListeningNetteComponents/Tests/ListeningNetteComponentsTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Pehapkari\Website\Tests\Posts\Year2017\ListeningNetteComponents\Tests;
+
+use Nette\Application\IPresenter;
+use Nette\Application\IPresenterFactory;
+use Nette\Application\Request;
+use Nette\Application\Responses\TextResponse;
+use Nette\Bridges\ApplicationLatte\Template;
+use Nette\DI\Container;
+use PHPUnit\Framework\TestCase;
+
+
+final class ListeningNetteComponentsTest extends TestCase
+{
+
+	const PRESENTER_NAME = 'Category';
+
+	/**
+	 * @var Container
+	 */
+	private $container;
+
+	/**
+	 * @var IPresenterFactory
+	 */
+	private $presenterFactory;
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function setUp()
+	{
+		$this->container = (new ContainerFactory)->create();
+		$this->presenterFactory = $this->container->getByType(IPresenterFactory::class);
+	}
+
+
+	public function testBasicRequest()
+	{
+		$request = new Request(self::PRESENTER_NAME, 'GET');
+		$presenter = $this->createPresenter();
+		$response = $presenter->run($request);
+
+		$this->assertInstanceOf(TextResponse::class, $response);
+		$this->assertInstanceOf(Template::class, $response->getSource());
+		$this->assertSame(file_get_contents(__DIR__ . '/responses/success/basic.request.txt'), (string) $response->getSource());
+	}
+
+
+	public function testAddToBasketFirstProductRequest()
+	{
+		$request = new Request(self::PRESENTER_NAME, 'GET', ['do' => 'addToBasket-1-add']);
+		$presenter = $this->createPresenter();
+		$response = $presenter->run($request);
+
+		$this->assertInstanceOf(TextResponse::class, $response);
+		$this->assertInstanceOf(Template::class, $response->getSource());
+		$this->assertSame(file_get_contents(__DIR__ . '/responses/success/add-to-basket-first-product.request.txt'), (string) $response->getSource());
+	}
+
+
+	public function testAddToBasketSecondProductRequest()
+	{
+		$request = new Request(self::PRESENTER_NAME, 'GET', ['do' => 'addToBasket-2-add']);
+		$presenter = $this->createPresenter();
+		$response = $presenter->run($request);
+
+		$this->assertInstanceOf(TextResponse::class, $response);
+		$this->assertInstanceOf(Template::class, $response->getSource());
+		$this->assertSame(file_get_contents(__DIR__ . '/responses/success/add-to-basket-second-product.request.txt'), (string) $response->getSource());
+	}
+
+
+	public function testAddToBasketThirdProductRequest()
+	{
+		$request = new Request(self::PRESENTER_NAME, 'GET', ['do' => 'addToBasket-3-add']);
+		$presenter = $this->createPresenter();
+		$response = $presenter->run($request);
+
+		$this->assertInstanceOf(TextResponse::class, $response);
+		$this->assertInstanceOf(Template::class, $response->getSource());
+		$this->assertSame(file_get_contents(__DIR__ . '/responses/success/add-to-basket-third-product.request.txt'), (string) $response->getSource());
+	}
+
+
+	/**
+	 * @return IPresenter
+	 */
+	protected function createPresenter()
+	{
+		$categoryPresenter = $this->presenterFactory->createPresenter(self::PRESENTER_NAME);
+		$categoryPresenter->autoCanonicalize = FALSE;
+
+		return $categoryPresenter;
+	}
+
+}
