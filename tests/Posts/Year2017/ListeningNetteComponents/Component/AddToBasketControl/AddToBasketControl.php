952462137c1d50cbffed133c8e5930ed945e9983
Tomas Pilar <email@tomaspilar.cz>
2017-01-13 17:28:24 +0100

    add component AddToBasketControl

---
+++ tests/Posts/Year2017/ListeningNetteComponents/Component/AddToBasketControl/AddToBasketControl.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Pehapkari\Website\Tests\Posts\Year2017\ListeningNetteComponents\Component\AddToBasketControl;
+
+use Nette\Application\UI\Control;
+use Pehapkari\Website\Tests\Posts\Year2017\ListeningNetteComponents\Event\ProductAddedToBasketEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+
+final class AddToBasketControl extends Control
+{
+
+	/**
+	 * @var array
+	 */
+	private $product;
+
+	/**
+	 * @var EventDispatcherInterface
+	 */
+	private $eventDispatcher;
+
+
+	public function __construct(array $product, EventDispatcherInterface $eventDispatcher)
+	{
+		$this->product = $product;
+		$this->eventDispatcher = $eventDispatcher;
+	}
+
+
+	public function handleAdd()
+	{
+		// There will be some logic with basket.
+		// e.g.: $this->basketFacade->addProduct($this->product);
+
+		// construct event object
+		$productAddedToBasketEvent = new ProductAddedToBasketEvent(
+			$this->product['id'],
+			$this->product['name'],
+			$this->product['price']
+		);
+		$this->eventDispatcher->dispatch(ProductAddedToBasketEvent::class, $productAddedToBasketEvent); // dispatch it!
+	}
+
+
+	public function render()
+	{
+		$this->template->setFile(__DIR__ . '/templates/default.latte');
+		$this->template->render();
+	}
+
+}
---
+++ tests/Posts/Year2017/ListeningNetteComponents/Component/AddToBasketControl/AddToBasketControlFactoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pehapkari\Website\Tests\Posts\Year2017\ListeningNetteComponents\Component\AddToBasketControl;
+
+
+interface AddToBasketControlFactoryInterface
+{
+
+	/**
+	 * @return AddToBasketControl
+	 */
+	function create(array $product);
+
+}
