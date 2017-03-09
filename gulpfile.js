fabdf35406944db62ef6e8918607f947e9c2199f
Adam Stipak <adam.stipak@gmail.com>
2017-03-09 14:20:28 +0100

    gulp: use gutils.log for generate nicer log to console

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,28 @@
 
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var gulputil = require('gulp-util');
 var exec = require('child_process').exec;
 
 gulp.task('default', function () {
     // Generate current version
     exec('vendor/bin/statie generate', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
+        gulputil.log(stdout);
+        gulputil.log(stderr);
     });
 
     // Run local server, open localhost:8000 in your browser
     exec('php -S 0.0.0.0:8000 -t output');
 
-    // For the second arg see: https://github.com/floatdrop/gulp-watch/issues/242#issuecomment-230209702
-    return watch(['source/**/*', '!**/*___jb_tmp___'], { ignoreInitial: false })
-        .on('change', function() {
+    gulp.watch(
+        // For the second arg see: https://github.com/floatdrop/gulp-watch/issues/242#issuecomment-230209702
+        ['source/**/*', '!**/*___jb_tmp___'],
+        { ignoreInitial: false },
+        function() {
             exec('vendor/bin/statie generate', function (err, stdout, stderr) {
-                console.log(stdout);
-                console.log(stderr);
+                gulputil.log(stdout);
+                gulputil.log(stderr);
             });
-        });
+        }
+    );
 });
